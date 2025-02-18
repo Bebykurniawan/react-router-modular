e1a6e1c20d6cd8d7248ef3010e9d4bc7c65b756b
Bebykurniawan <bebykurniawan2006@gmail.com>
2025-02-18 09:37:15 +0700

    feat: create a navLink in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,26 @@
+import { NavLink } from "react-router"
+
 const Navbar = () => {
+    interface link {
+        name: string,
+        to: string | number
+    }
+    const links: link[] = [
+        { name: "Home", to: "/" },
+        { name: "About", to: "/about" },
+      ];
+
   return (
-    <div>Navbar</div>
+    <nav className="navigation">
+    {links.map((link, index) => (
+      <span key={index} className="links">
+        <NavLink to={String(link.to)} className="a">
+          {link.name}
+        </NavLink>
+        {index < links.length - 1 && " | "}
+      </span>
+    ))}
+  </nav>
   )
 }
 
